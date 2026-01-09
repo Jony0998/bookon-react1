1de7517dd6988f9c1e484fb60c7b6305acd81a08
Jony0998 <bozorovjanobiddin9818@gmail.com>
2026-01-09 21:51:34 +0900

    docs: add documentation, tests and additional files

---
+++ training.ts
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ PROJECT STANDARTS:
+-Logging standarts
+- Naming standarts
+  function, method, variable => CAMEL  case    goHome
+  class => Paskal                               MemberServices
+  folder,file => KEBAB case
+  css => SNAKE case                              button_style
+-Error handling
+
+*/
+
+/*
+ Traditional Api
+ Rest Api
+ GraphQL
+
+*/
+ 
+/* Frontend development
+
+Traditional FD => BSSR(Admin) => EJS
+Modern FD  => SPA(User) => Json datani orqali =>  REACT   (library)
+
+*/
+
+
+/* 
+
+request join
+self destroy
+
+*/
+  /*
+  Validations
+
+Frontend validation
+Backend validation
+Database validation
+
+  */
