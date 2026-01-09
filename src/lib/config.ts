b856d59e0d345b6922f121ee664aa7c6af9f6ff1
Jony0998 <bozorovjanobiddin9818@gmail.com>
2026-01-09 21:50:42 +0900

    feat: add library types, enums and configuration

---
+++ src/lib/config.ts
@@ -0,0 +1,15 @@
+
+export const serverApi: string = `${process.env.REACT_APP_API_URL}`;
+
+export const Messages = {
+    error1: "Something went wrong!",
+    error2: "Please login first!",
+    error3: "Please fulfill all inputs!",
+    error4: "Message is empty",
+    error5: "Only images with jpeg, jpg, png format allowed!",
+    error6: "Username must be at least 3 characters long!",
+    error7: "Username can only contain letters, numbers, and underscores!",
+    error8: "Phone number must contain only digits!",
+    error9: "Phone number must be at least 9 digits long!",
+    error10: "Password must be at least 6 characters long!",
+};
---
+++ src/lib/enums/member.enum.ts
@@ -0,0 +1,11 @@
+export enum MemberType {
+  USER = "USER",
+  RESTAURANT = "RESTAURANT",
+  ADMIN = "ADMIN",
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE"
+}
